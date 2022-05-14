18e9d0ba91f92f2540c15394ff92af92b9fc711a
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-05-14 22:10:36 +0300

    browser-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { BrowserRouter, Route, Navigate } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Route path='/home' element={<div>home</div>} />
+        <Route path='/about' element={<div>about</div>} />
+        <Route path='/contacts' element={<div>contacts</div>} />
+        <Route path='*' element={<Navigate to={'/home'} />} />
+      </BrowserRouter>
     </div>
   );
 }
