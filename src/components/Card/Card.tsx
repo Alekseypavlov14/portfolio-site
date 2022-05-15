0ea38b6955f460f90aa27b75272820ec3f649846
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-05-15 21:20:11 +0300

    card

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,82 @@
+import { FC } from 'react'
+import { useSelector } from 'react-redux'
+import { themeSelector } from '../../features/theme/themeSlice'
+import cn from 'classnames'
+import styles from './Card.module.css'
+
+interface CardProps {
+  name: string,
+  description: string,
+  technologies: string[],
+  link: string,
+  repository: string,
+  date: string
+}
+
+const Card: FC<CardProps> = ({
+  name, 
+  description, 
+  technologies, 
+  link, 
+  repository, 
+  date, 
+  ...props
+}) => {
+  const theme = useSelector(themeSelector)
+
+  return (
+    <div className={cn(styles.Card, styles[theme])}>
+      <div className={styles.Name}>
+        {name}
+      </div>
+      <div className={styles.Description}>
+        {description}
+      </div>
+
+      <div className={styles.Links}>
+        <div className={styles.Link}>
+          <span className={styles.Word}>
+            Link: 
+          </span>
+          <a 
+            href={link} 
+            className={styles.Anchor}
+            target='_blank'
+            rel='noreferrer'
+          >
+            {link}
+          </a>
+        </div>
+        <div className={styles.Repository}>
+          <span className={styles.Word}>
+            Repo: 
+          </span>
+          <a 
+            href={repository} 
+            className={styles.Anchor}
+            target='_blank'
+            rel='noreferrer'
+          >
+            {repository}
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.Footer}>
+        <div className={styles.Technologies}>
+          {technologies.map((technology, index) => (
+            <span className={styles.Technology} key={index}>
+              {technology}
+            </span>
+          ))}
+        </div>
+
+        <div className={styles.Date}>
+          {date}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export { Card }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,17 @@
 import { FC } from 'react'
+import { Card } from '../../components/Card/Card'
 import styles from './Home.module.css'
+
+import projects from './../../data/projects.json'
 
 interface HomeProps {}
 
 const Home: FC<HomeProps> = (props) => {
   return (
     <div className={styles.Home}>
-      Home Page
+      {projects.map((project, index) => (
+        <Card {...project} />
+      ))}
     </div>
   )
 }
