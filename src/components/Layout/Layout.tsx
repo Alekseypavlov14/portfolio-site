8150a323de6948e1e9faf88c9bcc4a8b9e21962d
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-05-20 21:44:39 +0300

    add styles to Layout and solve the problem with page height

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,16 +2,23 @@ import { FC } from 'react'
 import { Header } from '../Header/Header'
 import { Footer } from './../Footer/Footer'
 import { Outlet } from 'react-router-dom'
+import styles from './Layout.module.css'
 
 interface LayoutProps {}
 
 const Layout: FC<LayoutProps> = () => {
   return (
-    <>
-      <Header />
-      <Outlet />
-      <Footer />
-    </>
+    <div className={styles.Layout}>
+      <div className={styles.Header}>
+        <Header />
+      </div>
+      <div className={styles.Outlet}>
+        <Outlet />
+      </div>
+      <div className={styles.Footer}>
+        <Footer />
+      </div>
+    </div>
   )
 }
 
