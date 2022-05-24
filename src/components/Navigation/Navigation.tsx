2be97dbcd6d85fac8737ff17162ed42497072b90
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-05-24 20:57:46 +0300

    clear non-used variables (Link in Navigation)

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { themeSelector } from '../../features/theme/themeSlice'
 import { Menu } from '../Menu/Menu'
