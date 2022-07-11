642ac8590ae9da285734c1685338fdcd07fb99fb
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-07-11 19:47:31 +0300

    add server-relations to contact page

--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -1,20 +1,35 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { Contact } from '../../components/Contact/Contact'
+import { IContact } from './../../types/Contact.interface'
 import { useSelector } from 'react-redux'
 import { themeSelector } from '../../features/theme/themeSlice'
-import styles from './Contacts.module.css'
+import { APIRequest } from '../../business/api/APIRequest'
 import element from './../../styles/Element.module.css'
-import contacts from './../../data/contacts.json'
 import cn from 'classnames'
+import styles from './Contacts.module.css'
 
 interface ContactsProps {}
 
 const Contacts: FC<ContactsProps> = () => {
   const theme = useSelector(themeSelector)
+  const [loading, setLoading] = useState(false)
+  const [contacts, setContacts] = useState<IContact[]>([])
+
+  useEffect(() => {
+    setLoading(true)
+    APIRequest('/contacts')
+    .then(data => setContacts(data.contacts))
+    .then(() => setLoading(false))
+  }, [])
 
   return (
     <div className={cn(element.Page, element[theme], styles[theme])}>
       <div className={element.Container}>
+        {loading && (
+          <div className={styles.Loading}>
+            loading ...
+          </div>
+        )}
         {contacts.map((contact, index) => (
           <Contact {...contact} key={index} />
         ))}
