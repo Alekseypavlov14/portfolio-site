dba2c9979ceef10c54e629704b02531b0e63ed38
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-07-11 20:44:01 +0300

    update project slice

--- src/features/project/projectSlice.ts
+++ src/features/project/projectSlice.ts
@@ -1,40 +1,39 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { query } from '../../business/query/Query'
 import { IProject } from '../../types/Project.interface'
 import { RootState } from '../../app/store'
 import { getTechnologies } from './calculateInitialState'
-import projects from '../../data/projects.json'
 
 interface InitialState {
-  readonly projects: IProject[],
-  readonly technologies: string[],
+  projects: IProject[],
+  technologies: string[],
   filteredProjects: IProject[],
-  SORT_KEYS: string[]
 }
 
 const initialState: InitialState = {
-  projects: projects,
-  filteredProjects: projects,
-  technologies: getTechnologies(projects),
-  SORT_KEYS: []
+  projects: [],
+  filteredProjects: [],
+  technologies: [],
 }
 
 const projectSlice = createSlice({
   name: 'project',
   initialState,
   reducers: {
-    sort(state, action) {
-      state.filteredProjects = query.sort(
-        state.projects, action.payload
-      )
-      state.SORT_KEYS = action.payload
+    addProjects(state, action: PayloadAction<IProject[]>) {
+      state.projects = action.payload
+      state.filteredProjects = action.payload
+      state.technologies = getTechnologies(action.payload)
+    },
+    sort(state, action: PayloadAction<string[]>) {
+      state.filteredProjects = 
+        query.sort(state.projects, action.payload)
     }
   }
 })
 
 export const projectReducer = projectSlice.reducer
-export const { sort } = projectSlice.actions
+export const { sort, addProjects } = projectSlice.actions
 export const projectsSelector = (state: RootState) => state.project.projects
 export const filteredProjectsSelector = (state: RootState) => state.project.filteredProjects
 export const technologiesSelector = (state: RootState) => state.project.technologies
-export const sortKeysSelector = (state: RootState) => state.project.SORT_KEYS
