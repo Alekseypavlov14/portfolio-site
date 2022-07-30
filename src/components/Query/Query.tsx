65a9a027e47a34bb943340783be10a9ecc0a15b7
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-07-30 17:54:34 +0300

    remake Query component

--- src/components/Query/Query.tsx
+++ src/components/Query/Query.tsx
@@ -1,10 +1,9 @@
-import { FC, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { filteredProjectsSelector, sort } from './../../features/project/projectSlice'
+import { FC, Dispatch, SetStateAction } from 'react'
+import { useSelector } from 'react-redux'
 import { themeSelector } from '../../features/theme/themeSlice'
-import cn from 'classnames'
 import Select from 'react-select'
 import styles from './Query.module.css'
+import cn from 'classnames'
 import './Select.scss'
 
 interface Option {
@@ -14,36 +13,24 @@ interface Option {
 
 interface QueryProps {
   options: Option[]
+  setTechnologies: Dispatch<SetStateAction<string[]>>
 }
 
-const Query: FC<QueryProps> = ({ options }) => {
+const Query: FC<QueryProps> = ({ 
+  options, 
+  setTechnologies
+}) => {
   const theme = useSelector(themeSelector)
-  const dispatch = useDispatch()
   
-  const [isChosen, setChosen] = useState(false)
-  const filteredProjects = useSelector(filteredProjectsSelector)
-
   return (
     <div className={cn(styles.Query, styles[theme])}>
       <Select 
+        onChange={(e) => setTechnologies(e.map((option) => option.value))}
         classNamePrefix={'select__' + theme}
         placeholder='Select tools...'
         options={options}
         isMulti
-        onChange={(e) => {
-          const technologies = e.map((option) => option.value)
-          dispatch(sort(technologies))
-          setChosen(Boolean(e.length))
-        }}
       />
-
-      {isChosen && (
-        <div className={styles.ResultAmount}>
-          {filteredProjects.length === 1
-          ? `${filteredProjects.length} project was found`
-          : `${filteredProjects.length} projects were found`}
-        </div>
-      )}
     </div>
   )
 }
