7cbc469c7a75401d16098f574f2b1bce82589061
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-07-30 19:20:40 +0300

    add BurgerButton component

---
+++ src/components/BurgerButton/BurgerButton.tsx
@@ -0,0 +1,26 @@
+import { FC } from 'react'
+import { isMenuOpened, toggle } from '../../features/burger/burgerSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { themeSelector } from '../../features/theme/themeSlice'
+import cn from 'classnames'
+import styles from './BurgerButton.module.css'
+
+interface BurgerButtonProps {}
+
+const BurgerButton: FC<BurgerButtonProps> = () => {
+  const dispatch = useDispatch()
+
+  const theme = useSelector(themeSelector)
+  const isOpened = useSelector(isMenuOpened)
+
+  return (
+    <div 
+      className={cn(styles.BurgerButton, styles[theme], isOpened && styles.Clicked)}
+      onClick={() => dispatch(toggle())}
+    >
+      <span />
+    </div>
+  )
+}
+
+export { BurgerButton }
